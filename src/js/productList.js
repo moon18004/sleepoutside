371347233cd0a04373d9e6aaf2a53688ddb26f3b
JaxonAtBYUI <91147570+JaxonAtBYUI@users.noreply.github.com>
2022-10-19 12:59:06 -0600

    added dicount to front page

--- src/js/productList.js
+++ src/js/productList.js
@@ -28,8 +28,13 @@ export default class ProductListing {
     template.querySelector("img").alt += product.Name;
     template.querySelector(".card__brand").innerHTML = product.Brand.Name;
     template.querySelector(".card__name").innerHTML = product.NameWithoutBrand;
-    template.querySelector(".product-card__price").innerHTML +=
-      product.ListPrice;
+    template.querySelector(".product-card__price").innerHTML += product.ListPrice;
+    
+    if (product.SuggestedRetailPrice > product.ListPrice) {
+      let percentOff = Math.round(100 * (product.SuggestedRetailPrice - product.ListPrice) / product.SuggestedRetailPrice)
+      template.querySelector(".product-card__discount").innerHTML = `${percentOff}% Off`
+    }
+
     return template;
   }
 }
